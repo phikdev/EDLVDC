d3bdbd1d92fe070868bd93534ab3845ce64b794e
phik <phik.dev@gmail.com>
2024-05-07 08:47:37 +0200

    construction de home page

--- themes/EDLVDC/index.php
+++ themes/EDLVDC/index.php
@@ -5,4 +5,24 @@
     </div>
     
 </section>
+<section class="logo">
+<img class="photoX" src="<?php echo get_stylesheet_directory_uri().'/images/logoX.png' ?>" alt="photoX">
+</section>
+<section class="description">
+    <p>
+    Bienvenue sur le site de l'élevage de la vallée de Cheyenne !</p>
+
+    <p>Nous sommes fiers de vous présenter notre passion pour cette race majestueuse et attachante qu'est le Léonberg. Situé au cœur de la campagne, notre élevage est un lieu où nos chiens sont élevés avec amour, soin et dévouement.</p>
+
+    <p>Nous sommes disponibles pour répondre à toutes vos questions et vous guider dans le choix du compagnon idéal en fonction de votre style de vie et de vos préférences personnelles. Que vous cherchiez un chien de compagnie, un partenaire d'activités ou un compétiteur en exposition canine, nous avons le Léonberg parfait pour vous.</p>
+
+    <p>En parcourant notre galerie de photos, vous pourrez admirer la beauté et la grâce de nos chiens, ainsi que la complicité qui nous lie à eux. Chaque chien que nous élevons est une partie de notre famille, et nous sommes fiers de partager notre passion avec vous.</p>
+
+    <p>N'hésitez pas à nous contacter pour toute demande d'information supplémentaire ou pour planifier une visite de notre élevage. Nous serions ravis de vous accueillir et de vous faire découvrir le merveilleux monde des Léonberg.</p>
+
+    <p>Merci de votre intérêt pour notre élevage, et à bientôt sur le site de la vallée de Cheyenne !</p>
+
+
+    </p>
+</section>
 <?php get_footer();?>
